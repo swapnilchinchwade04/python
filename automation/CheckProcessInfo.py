965e1570b9ffae72d35fd2c133d2be80ab643dfc
swapnilchinchwade04 <swapnilchinchwade04@gmail.com>
2026-05-26 17:03:43 +0530

    Find Process Information

--- automation/CheckProcessInfo.py
+++ automation/CheckProcessInfo.py
@@ -1,8 +1,8 @@
-# Design automation script which display information of running processes as its name, PID,
-# Username.
-# Usage : ProcInfo.py
+# Design automation script which accept process name and display information of that process if
+# it is running.
+# Usage : CheckProcessInfo.py Notepad
 
-from sys import *
+from sys import argv
 import psutil
 
 def main():
@@ -10,7 +10,7 @@ def main():
     print("Script Name:", argv[0])
     print("No of arguments:", len(argv))
 
-    if len(argv) < 1 :
+    if len(argv) < 1 or len(argv) > 2:
         print("Invalid number of arguments.")
         print("Use -u for help.")
         print("Use -h for help.")
@@ -23,10 +23,20 @@ def main():
             print("Script ot display information of running process.")
             exit()
 
-    if len(argv) == 1:
+    if len(argv) == 2:
         try:
+            flag = 0
             for proc in psutil.process_iter(['pid', 'name', 'username']):
-                print(f"PID: {proc.info['pid']} - Name: {proc.info['name']} - User Name: {proc.info['username']}")
+                app_name = proc.info['name'].split(".")[0]
+                #print("Proc name: ",proc.info['name'])
+                #print("Argv name: ", app_name)
+                if app_name == argv[1]:
+                    #print("App name: ", app_name)
+                    print(f"PID: {proc.info['pid']} - Name: {proc.info['name']} - User Name: {proc.info['username']}")
+                    flag = 1
+            if flag == 0:
+                print("Process not found.")
+
 
         except Exception:
             print(Exception)
